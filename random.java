65f545be137c2ccd872a3ed80f37c9825181c87a
Evan Suslovich <evan.suslovich@lincolnps.org>
2020-06-17 13:43:45 -0400

    more practice

--- random.java
+++ random.java
@@ -1,5 +1,5 @@
 public class random{
     public static void main(String[] args) {
-        System.out.println("This is random code for learning github");
+        System.out.println("This is random code for learning github. Changed this blah");
     }
 }
